2acca2b715db2ab6158f62829f32c5d97d6d3274
Alfredo Aguiar Arce <alfredoaguiararce@gmail.com>
2023-02-20 23:49:23 -0700

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ app.get('/', (req, res) => {
     res.send('Hola mi server en express.');
 });
 
+app.get('/nueva-ruta',(req, res) => {
+    res.send('Hola soy una nueva ruta');
+});
+
+app.get('/products',(req, res) => {
+    res.json({
+        price: 10000,
+        name: 'Name 1'
+    });
+});
+
 app.listen(port, () => {
    console.log('Mi port : ' + port); 
 });
